9e5cf249e23ad7ab8f647587fcdddb29c60f5ad4
Asim Ali Murtaza <mo.asim.murtaza@gmail.com>
2025-05-04 00:06:20 +0500

    S#9 Added Automated threat response for suspicious activities, failed login attempts, temporary account suspension

---
+++ src/app/api/account/unblock/route.ts
@@ -0,0 +1,31 @@
+// app/api/account/unblock/route.ts
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongodb";
+import User from "@/models/User";
+
+export async function POST(req: Request) {
+  const { token } = await req.json();
+  await dbConnect();
+
+  const user = await User.findOne({
+    unblockToken: token,
+    unblockTokenExpires: { $gt: Date.now() }, // not expired
+  });
+
+  if (!user) {
+    return NextResponse.json(
+      { message: "Invalid or expired token" },
+      { status: 400 }
+    );
+  }
+
+  user.isAccountLocked = false;
+  user.loginAttempts = 0;
+  user.unblockToken = null;
+  user.unblockTokenExpires = null;
+  await user.save();
+
+  return NextResponse.json({
+    message: "Your account has been successfully unblocked!",
+  });
+}
